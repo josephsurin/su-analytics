eb499a5d755e1fbe88e32061d1455ab3ba1033ab
Joseph Surin <joseph.surin@gmail.com>
2019-03-01 13:55:48 +1100

    add browser script

---
+++ browser-script/su-analytics.js
@@ -0,0 +1,19 @@
+(function suAnalyticsInitialise() {
+	const API_URL = 'http://su-analytics.herokuapp.com/api'
+	window.onhashchange(() => {
+		var url = window.location.href
+		if(url[url.length - 1] == '/') url = url.slice(0, url.length - 1)
+		var postBody = {
+			query: `mutation {
+				addVisit(url: ${url}) {
+					_id
+				}
+			}`
+		}
+		fetch(API_URL, {
+			method: 'POST',
+			body: JSON.stringify(postBody),
+			headers: { 'Content-Type': 'application/json' }
+		}).then(() => {})
+	})
+})()
